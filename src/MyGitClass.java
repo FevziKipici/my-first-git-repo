8e313379e717920f66981b83c8bed817a4f29027
FevziKipici <109439440+FevziKipici@users.noreply.github.com>
2022-07-16 23:04:12 -0400

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -3,5 +3,6 @@ public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("Version 1");
         System.out.println("Hello world");
+        System.out.println("I am Fevzi");
     }
 }
